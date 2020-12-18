5b0603ebd2da42230abeac5837495d878dd4a247
sguigom541 <2daasguigom21@ieslasfuentezuelas.com>
2020-12-18 02:34:16 +0100

    sin terminar

--- proyectoDWEC/script/libreria.js
+++ proyectoDWEC/script/libreria.js
@@ -1,55 +1,95 @@
 window.addEventListener("load",function(){
     // manejador de evento para la ventana modal de crear trabajador
-
+    var btnNewEmpresa=document.getElementById("menuEmpresa");
+    var btnMenuUsuarios=document.getElementById("menuUsuarios");
+    var btnPuestosDisponibles=document.getElementById("menuPuestosDisponibles");
+    var btnEstadisticasEmpresa=document.getElementById("menuEstadisticasEmpresa");
+    var btnFichar=document.getElementById("menuFichar");
     var btnModalNewTrabajador = document.getElementById("modal-newUser");
     var divModalTrabajador = document.getElementById("div-nuevoTrabajador");
-    var btnCerrarModalMascota = document.getElementById("btnCerrarNewTrabajador");
-
+    var btnCerrarModalTrabajador = document.getElementById("btnCerrarNewTrabajador");
+    const aside=document.getElementById("menuTrabajadores");
+    const main=aside.parentNode.children[1].children;
     btnModalNewTrabajador.addEventListener("click", function () {
         divModalTrabajador.style.display = "block";
 
-        body.style.position = "static";
-        body.style.height = "100%";
-        body.style.overflow = "hidden";
+        document.body.style.position = "static";
+        document.body.style.height = "100%";
+        document.body.style.overflow = "hidden";
     })
 
-    btnCerrarModalMascota.addEventListener("click", function () {
+    btnCerrarModalTrabajador.addEventListener("click", function () {
         divModalTrabajador.style.display = "none";
 
-        body.style.position = "inherit";
-        body.style.height = "auto";
-        body.style.overflow = "visible";
+        document.body.style.position = "inherit";
+        document.body.style.height = "auto";
+        document.body.style.overflow = "visible";
     })
 
     // manejadores de evento de los divs de la empresa
 
-
     
-
-
-
-
-
-
-
-
-
-
-    function toggle(clase, objeto) {
-        var clasesActuales = objeto.getAttribute("class");
-        if (!clasesActuales) {
-            nuevaClase = clase;
-        }
-        else {
-            var expReg = new RegExp("(^" + clase + "$|^" + clase + " | " + clase + " | " + clase + "$)");
-            var nuevaClase = clasesActuales.replace(expReg, "");
-            if (clasesActuales == nuevaClase) {
-                nuevaClase += " " + clase;
-               
+    btnNewEmpresa.addEventListener("click",function(){
+        for (let i = 0; i < main.length; i++) {
+            if(main[i].getAttribute("id")=="divEmpresa"){
+                main[i].style.display="block";
             }
+            else{
+                main[i].style.display="none";
+            }
+            
         }
-        objeto.setAttribute("class", nuevaClase);
-    }
+    })
 
+
+   
+    btnMenuUsuarios.addEventListener("click",function(){
+        for (let i = 0; i < main.length; i++) {
+            if(main[i].getAttribute("id")=="divTrabajadores"){
+                main[i].style.display="block";
+            }
+            else{
+                main[i].style.display="none";
+            }
+            
+        }
+    })
+    
+    btnPuestosDisponibles.addEventListener("click",function(){
+        for (let i = 0; i < main.length; i++) {
+            if(main[i].getAttribute("id")=="divPuestosDisponibles"){
+                main[i].style.display="block";
+            }
+            else{
+                main[i].style.display="none";
+            }
+            
+        }
+    })
+
+
+    btnEstadisticasEmpresa.addEventListener("click",function(){
+        for (let i = 0; i < main.length; i++) {
+            if(main[i].getAttribute("id")=="divEstadisticas"){
+                main[i].style.display="block";
+            }
+            else{
+                main[i].style.display="none";
+            }
+            
+        }
+    })
+
+    btnFichar.addEventListener("click",function(){
+        for (let i = 0; i < main.length; i++) {
+            if(main[i].getAttribute("id")=="menuFichar"){
+                main[i].style.display="block";
+            }
+            else{
+                main[i].style.display="none";
+            }
+            
+        }
+    })
     
 })
--- proyectoDWEC/script/main.js
+++ proyectoDWEC/script/main.js
@@ -1,34 +1,166 @@
-window.addEventListener("load",function(){
-    importarScript("./script/libreria.js");
-    importarScript("./script/empresa.js");
-     // Variables
-    const btnCrearEmpresa=document.getElementById("crearEmpresa");
-    const btnCrearTrabajador=document.getElementById("crearTrabajador");
-    const btnAcceso=document.getElementById("acceso");
-    const btnModTrabajador=document.getElementById("modEmpleado");
-    const btnBorrarTrabajador=document.getElementById("borrarEmpleado");
+window.addEventListener("load", function () {
+    // Variables
+    const btnCrearEmpresa = document.getElementById("crearEmpresa");
+    const btnCrearTrabajador = document.getElementById("crearTrabajador");
+    const btnAcceso = document.getElementById("acceso");
+    const btnEditarEmpleado = document.getElementById("btnEditar");
+    const btnBorrarEmpleado = document.getElementById("btnBorrar");
 
-    btnCrearEmpresa.addEventListener("click",function(){
-        var header=document.getElementsByClassName("header");
-        var formRegEmpresa=this.parentNode.parentNode;
-        var inputs=formRegEmpresa.firstElementChild.getElementsByTagName("input");
-        var nombreEmpresa=inputs[0].value;
-        var horaInicio=inputs[1].value;
-        var horaFin=inputs[2].value;
-        var numTrabSimul=inputs[3].value;
-        
-        var em=new empresa(nombreEmpresa,horaInicio,horaFin,numTrabSimul);
-        grabarEmpresa(em,"empresa");
-        header[0].children[0].innerText=em.nombre;
+
+
+
+    // recuperamos los datos de la empresa y los mostramos
+    mostrarTrabajadores();
+
+
+
+    btnCrearEmpresa.addEventListener("click", function () 
+    {
+        var header = document.getElementsByClassName("header");
+        var formRegEmpresa = this.parentNode.parentNode;
+        var inputs = formRegEmpresa.firstElementChild.getElementsByTagName("input");
+        var nombreEmpresa = inputs[0].value;
+        var horaInicio = inputs[1].value;
+        var horaFin = inputs[2].value;
+        var numTrabSimul = inputs[3].value;
+
+        var em = new empresa(nombreEmpresa, numTrabSimul);
+        em.horario.push(horaInicio);
+        em.horario.push(horaFin);
+        grabarEmpresa(em, "empresa");
+        header[0].children[0].innerText = em.nombre;
     })
-    btnCrearTrabajador.addEventListener("click",function(){
+    btnCrearTrabajador.addEventListener("click", function () 
+    {
+        var formNewTrab = document.getElementById("newTrabajador");
+        var inputs = formNewTrab.firstElementChild.getElementsByTagName("input");
+        var dni = inputs[0].value;
+        var nombre = inputs[1].value;
+        var horasContratadas = inputs[2].value;
+        var table = document.getElementById("trabajadores");
+        var tbody = table.children[1];
+        var row = tbody.insertRow(0);
+        var celda;
+        celda = row.insertCell(0);
+        celda.innerText = dni;
+        celda = row.insertCell(1);
+        celda.innerText = nombre;
+        celda = row.insertCell(2);
+        celda.innertText = horasContratadas;
+        celda = row.insertCell(3);
+        var modifica = document.createElement("button");
+        modifica.textContent = "Editar";
+        modifica.id = "btnEditar";
+        modifica.classList = "modtrabajador";
+        celda.appendChild(modifica);
+        celda = row.insertCell(4);
+        var elimina = document.createElement("button");
+        elimina.textContent = "Eliminar";
+        elimina.id = "btnEliminar";
+        elimina.classList = "borrartrabajador";
+        celda.appendChild(elimina);
+
+        var empre = recuperarDatos();
+
+        empre.trabajadores.push(new trabajador(dni, nombre, horasContratadas));
+        grabarDatos(empre);
+        mostrarTrabajadores();
+        //cerramos el div modal de crear usuario
+        var divModalTrabajador = document.getElementById("div-nuevoTrabajador");
+        divModalTrabajador.style.display = "none";
+
+        document.body.style.position = "inherit";
+        document.body.style.height = "auto";
+        document.body.style.overflow = "visible";
 
     })
-    btnAcceso.addEventListener("click",function(){
+        btnEditarEmpleado.addEventListener("click", function () 
+        {
+            var tr = this.parentNode.parentNode;
+            var tds = tr.children;
+            var dni = tds[0].textContent;
+            var nombre = tds[1].textContent;
+            var horario = tds[2].textContent;
+            var table = document.getElementById("trabajadores");
+            var tbody = table.children[1];
+            var tfoot = table.children[2];
+            var trTfoot=tfoot.children[0];
+            var inputsTfoot=document.querySelectorAll("tfoot input");
+            var posicion=this.parentNode.parentNode.sectionRowIndex;
+
+            arrayTrabajadores.splice(posicion,1);
+            tbody.deleteRow(posicion);
+            inputsTfoot[0].innerText=dni;
+            inputsTfoot[1].innetText=nombre;
+            inputsTfoot[2].innerText=horario;
+        })
+
+   
+    
+
+    
+   
+    
+
+   
+    
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    btnAcceso.addEventListener("click", function () {
 
     })
 
 })
+
+function mostrarTrabajadores() {
+    var respuesta = "";
+    var table = document.getElementById("trabajadores");
+    var tbody = table.children[1];
+    var empresa = recuperarDatos();
+    var numTrabajadores = empresa.trabajadores.length;
+    for (let i = 0; i < numTrabajadores; i++) {
+        respuesta += "<tr><td>";
+        respuesta += empresa.trabajadores[i].dni;
+        respuesta += "</td><td>";
+        respuesta += empresa.trabajadores[i].nombre;
+        respuesta += "</td><td>";
+        respuesta += empresa.trabajadores[i].contrato;
+        respuesta += "</td><td>";
+        respuesta += "<button id='btnEditar' class='modtrabajador'>Editar</button>";
+        respuesta += "</td><td>";
+        respuesta += "<button id='btnEliminar' class='borrartrabajador'>Borrar</button>";
+        respuesta += "</td></tr>";
+
+
+    }
+    tbody.innerHTML = respuesta; //codigo fuente HTML de la tabla
+}
+
+
+
+
+
 
 /**
  * Guarda la empresa en el localStorage
@@ -36,59 +168,47 @@ window.addEventListener("load",function(){
  * @param {string} nombreCadena 
  * 
  */
-function grabarEmpresa(objetoempresa){
+function grabarDatos(objetoempresa) {
     var empresaCadena;
-    empresaCadena=JSON.stringify(objetoempresa);
-    localStorage.setItem("empresa",empresaCadena);
+    empresaCadena = JSON.stringify(objetoempresa);
+    localStorage.setItem("empresa", empresaCadena);
 }
 /**
  * @return {empresa} retorna el objeto empresa
  */
-function recuperarEmpresa(){
-    var cadena= localStorage.getItem();
-    if(cadena){
-        var objetoEmpresa=JSON.parse(cadena);
-        var nombreEmpresa=objetoEmpresa.nombre;
-        var horaIn=objetoEmpresa.horaInicio;
-        var horaFN=objetoEmpresa.horaFin;
-        var numSimul=objetoEmpresa.numTrabSimul;
-        var numTrabajadores=objetoEmpresa.trabajadores.length;
-        var horario=objetoEmpresa.horario;
-        var lengthHorario=horario.length;
-        var empre=new empresa(nombreEmpresa,horaIn,horaFN,numSimul);
+function recuperarDatos() {
+    var cadena = localStorage.getItem("empresa");
+    if (cadena) {
+        var objetoEmpresa = JSON.parse(cadena);
+        var nombreEmpresa = objetoEmpresa.nombre;
+        var numSimul = objetoEmpresa.numTrabSimul;
+        var numTrabajadores = objetoEmpresa.trabajadores.length;
+        var horario = objetoEmpresa.horario;
+        var lengthHorario = horario.length;
+        var empre = new empresa(nombreEmpresa, numSimul);
 
 
         for (let i = 0; i < lengthHorario; i++) {
-            var hora=objetoEmpresa.horario[i];
+            var hora = objetoEmpresa.horario[i];
             empre.horario.push(hora);
-            
+
         }
         for (let i = 0; i < numTrabajadores; i++) {
-            var trabajadorLocal=objetoEmpresa.trabajadores[i];
-            var dni=trabajadorLocal.dni;
-            var nombre=trabajadorLocal.nombre;
-            var horasContratada=trabajadorLocal.contrato
-            var empleado=new trabajador(dni,nombre,horasContratada)
+            var trabajadorLocal = objetoEmpresa.trabajadores[i];
+            var dni = trabajadorLocal.dni;
+            var nombre = trabajadorLocal.nombre;
+            var horasContratada = trabajadorLocal.contrato
+            var empleado = new trabajador(dni, nombre, horasContratada)
             empre.trabajadores.push(empleado);
-            
+
         }
-        
+
         return empre;
 
-       
+
     }
-    else{
-        var empre=new empresa("","");
+    else {
+        var empre = new empresa("", "");
     }
     return empre;
-}
-
-/**
- * 
- * @param {string} nombre la url del script a importar 
- */
-function importarScript(nombre) {
-    var s = document.createElement("script");
-    s.src = nombre;
-    document.querySelector("head").appendChild(s);
 }
